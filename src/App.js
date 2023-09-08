dbc444414abf9000ae25dea58b9ba95653271e37
tri w <triw@oninyon.com>
2023-09-08 20:54:10 +0700

    store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/main.css'
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './component/navbar/Navbar';
-import Login from './pages/login/index'
-import Register from './container/register/Register';
-import Dashboard from './pages/dashboard/index';
-import TopUp from './pages/topup/index'
-import Pay from './pages/pay/index'
-import Transaction from './pages/transaction/index'
-import Account from './pages/account/index'
 
 import Route from './route/Route';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 function App() {
   return (
     <main>
-      <Route />
+      <Provider store={store}>
+        <Route />
+      </Provider>
     </main>
   );
 }
