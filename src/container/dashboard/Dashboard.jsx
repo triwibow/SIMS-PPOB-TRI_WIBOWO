1b01c6e9180343b2c93b400de98bef6b17ff8672
Tri Wibowo <triw@oninyon.com>
2023-09-08 14:43:00 +0700

    - sd

--- src/container/dashboard/Dashboard.jsx
+++ src/container/dashboard/Dashboard.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import PhotoProfile from './component/PhotoProfile'
+import saldoBg from '../../assets/images/bg-saldo.png'
+import TextButton from '../../component/button/TextButton'
+import { IoEyeOutline } from 'react-icons/io5'
 
 const Dashboard = () => {
   return(
     <div className='container mt-4'>
       <div className='row'>
-        <div className='col-md-4'>
+        <div className='col-md-6'>
           <PhotoProfile />
           <p className='mb-3 text-app-dark text-head-4'>Selamat Datang,</p>
           <h1 className='text-app-dark text-head-2 fw-bold'>Kristanto Wibowo</h1>
         </div>
-        <div className='col-md-8'>
-
+        <div className='col-md-6'>
+          <div className='saldo-wrapper'>
+            <div className='z-index-100'>
+              <span className='text-app-white mb-3 d-block'>
+                Saldo anda
+              </span>
+              <h2 className='text-app-white text-head-4 fw-bold mb-3'>Rp 1.000.000</h2>
+              <div className='d-flex align-items-center'>
+                <TextButton 
+                  text="Lihat saldo"
+                  className="text-app-white text-app-md me-2"
+                />
+                <IoEyeOutline color='#FFFFFF' />
+              </div>
+            </div>
+            <img 
+              src={saldoBg} 
+              alt="bg-saldo"
+              className='img-fluid'
+            />
+          </div>
         </div>
       </div>
     </div>
