c2a79a2f7a3c0eae2b19ca788f57cba12583fb69
tri w <triw@oninyon.com>
2023-09-10 10:07:40 +0700

    - grab

--- src/container/dashboard/slider/Slider.jsx
+++ src/container/dashboard/slider/Slider.jsx
@@ -1,89 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5'
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
 import Banner from './component/Banner'
-
-
-const SwiperButtonPrev = () => {
-  const swiper = useSwiper();
-  return (
-    <button className='button-transparent' onClick={() => swiper.slidePrev()}>
-      <IoChevronBackOutline className="text-xl" />
-    </button>
-  )
-}
-
-const SwiperButtonNext = () => {
-  const swiper = useSwiper();
-  return (
-    <button className='button-transparent' onClick={() => swiper.slideNext()}>
-      <IoChevronForwardOutline className="text-xl" />
-    </button>
-  )
-}
+import { fetchData } from '../../../store/actions/bannerAction'
+import {modalError} from '../../../store/actions/modalAction'
+import { useDispatch, useSelector } from 'react-redux';
 
 const Slider = () => {
+  const dispatch = useDispatch()
+  const { status, message, banner } = useSelector(state => state.banner)
   const [width, setWidth] = useState(0)
   const [slidesPerView, setSlidesPerView] = useState(4)
 
-  const data = [
-    {
-      id:"1",
-      user:"Golden Man 1",
-      username:"@batman",
-      current_bid:"112.4 ETH",
-    },
-    {
-      id:"2",
-      user:"Golden Man 2",
-      username:"@batman",
-      current_bid:"112.4 ETH",
-    },
-    {
-      id:"3",
-      user:"Golden Man 3",
-      username:"@batman",
-      current_bid:"112.4 ETH",
-    },
-    {
-      id:"4",
-      user:"Golden Man 4",
-      username:"@batman",
-      current_bid:"11.4 BTC",
-    },
-    {
-      id:"5",
-      user:"Golden Man 5",
-      username:"@batman",
-      current_bid:"11.4 BTC",
-      
-    },
-    {
-      id:"6",
-      user:"Golden Man 6",
-      username:"@batman",
-      current_bid:"11.4 BTC",
-    },
-    {
-      id:"7",
-      user:"Golden Man 6",
-      username:"@batman",
-      current_bid:"11.4 BTC",
-    },
-    {
-      id:"8",
-      user:"Golden Man 6",
-      username:"@batman",
-      current_bid:"11.4 BTC",
-    }
-
-  ]
-
-  const _renderData = (item) => {
+  const _renderData = (item, index) => {
     return (
-      <div className='col-md-3' key={item.id}>
-        <SwiperSlide key={item.id}>
+      <div className='col-md-4' key={index}>
+        <SwiperSlide key={index}>
           <Banner 
             data={item}
           />
@@ -96,23 +28,14 @@ const Slider = () => {
     if(typeof window != undefined){
       setWidth(window.innerWidth)
     }
-  }, [])
 
-  useEffect(() => {
-  
-    if(width <= 576){
-      setSlidesPerView(1)
-    } else if(width <= 768){
-      setSlidesPerView(2)
-    } else {
-      setSlidesPerView(4)
-    }
-  }, [width])
+    dispatch(fetchData())
+  }, [])
 
   return (
     <div className='container mt-5'>
       <Swiper
-        spaceBetween={20}
+        spaceBetween={5}
         slidesPerView={slidesPerView}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
@@ -127,8 +50,8 @@ const Slider = () => {
         
         {width > 0? (
           <div className='row'>
-            {data.map((item) => {
-              return _renderData(item)
+            {banner.map((item, index) => {
+              return _renderData(item, index)
             })}
           </div>
         ):(
--- src/container/dashboard/slider/component/Banner.jsx
+++ src/container/dashboard/slider/component/Banner.jsx
@@ -1,36 +1,16 @@
 import React from "react"
-import { IoHeartOutline, IoTimeOutline } from 'react-icons/io5'
 
-const CardProduct = (props) => {
+const Banner = (props) => {
 
   const { data } = props
 
   return (
-    <div className="card-product">
-      <div className="card-product-body">
-        <div className="card-product-thumbnail mb-4">
-          <button className="card-product-like">
-            <IoHeartOutline /> 24
-          </button>
-          
-          <span className="card-product-timer">
-            <IoTimeOutline className="me-2" /> 20:12:10
-          </span>
-        </div>
-        <h3 className="text-primary fw-bold mb-2">{data.user}</h3>
-        <div className="d-flex align-items-center">
-         
-          <small className="text-sm fw-bold ms-2">{data.username}</small>
-        </div>
-      </div>
-      <div className="card-product-footer">
-        <div>
-          <small className="text-xs mb-1 d-block">CURRENT BID</small>
-          <span className="text-md fw-bold">{data.current_bid}</span>
-        </div>
-      </div>
-    </div>
+		<img 
+			src={data.banner_image} 
+			alt="banner"
+			style={{cursor:'grab'}}
+		/>
   );
 }
 
-export default CardProduct
+export default Banner
