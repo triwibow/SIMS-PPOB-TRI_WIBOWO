583f1ae61e10fb483ca175e74f1880283409b7c4
tri w <triw@oninyon.com>
2023-09-10 15:34:33 +0700

    - upload

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/main.css'
 
-
+import { useEffect } from 'react';
 import RouteApp from './route/Route';
 import { Provider } from 'react-redux';
 import store from './store';
 
 
 function App() {
+  useEffect(() => {
+    document.title = 'SIMS PPOB-TRI WIBOWO'
+  }, [])
   return (
     <main>
       <Provider store={store}>
